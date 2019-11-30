50feda83f396eb82ee164fe3d7e06ded4656317d
Alexander Chernoskutov <alecherov@gmail.com>
2019-11-30 18:07:15 +0500

    using _crc16_update() from avr-libc

--- CRC.h
+++ CRC.h
@@ -2,14 +2,18 @@
 #ifndef CRC_ARDUINO_H
 #define CRC_ARDUINO_H
 
+#ifdef __AVR__
+#include <util/crc16.h>
+#endif
+
 namespace CRC
 {
     uint8_t crc8(const uint8_t *buff, size_t size)
     {
         uint8_t* p = (uint8_t*)buff;
-        uint8_t result = 0xFF;
+        uint8_t result = 0;
 
-        for (result = 0 ; size != 0 ; size--)
+        for (; size != 0 ; size--)
         {
             result ^= *p++;
 
@@ -31,17 +35,20 @@ namespace CRC
 
     uint16_t crc16(const uint8_t* buff, size_t size)
     {
-        uint8_t* data = (uint8_t*)buff;
         uint16_t result = 0xFFFF;
 
         for (size_t i = 0; i < size; ++i)
         {
-            result ^= data[i];
+#ifdef __AVR__
+            result = _crc16_update(result, buff[i]);
+#else
+            result ^= buff[i];
             for (size_t j = 0; j < 8; ++j)
             {
                 if (result & 0x01) result = (result >> 1) ^ 0xA001;
                 else result >>= 1;
             }
+#endif
         }
         return result;
     }
