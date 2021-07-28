e482eb2bff98f9cd884c14d74d322dfce99cfd69
Hideaki Tai <hideaki.tai@gmail.com>
2021-07-28 19:23:34 +0900

    Add continuous (update) calcuration

--- src/CRCx.h
+++ src/CRCx.h
@@ -68,6 +68,66 @@ namespace util {
                 return 0xFFFFFFFF;
             }
 
+            inline uint8_t crc8_update(const uint8_t* data, const size_t size, const Crc8 type = Crc8::SMBUS) {
+                static FastCRC8 fastcrc;
+                static bool has_initialized {false};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    switch (type) {
+                        case Crc8::SMBUS: return fastcrc.smbus(data, size);
+                        case Crc8::MAXIM: return fastcrc.maxim(data, size);
+                    }
+                } else {
+                    switch (type) {
+                        case Crc8::SMBUS: return fastcrc.smbus_upd(data, size);
+                        case Crc8::MAXIM: return fastcrc.maxim_upd(data, size);
+                    }
+                }
+                return 0xFF;
+            }
+
+            inline uint16_t crc16_update(const uint8_t* data, const size_t size, const Crc16 type = Crc16::MODBUS) {
+                static FastCRC16 fastcrc;
+                static bool has_initialized {false};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    switch (type) {
+                        case Crc16::CCITT: return fastcrc.ccitt(data, size);
+                        case Crc16::KERMIT: return fastcrc.kermit(data, size);
+                        case Crc16::MODBUS: return fastcrc.modbus(data, size);
+                        case Crc16::XMODEM: return fastcrc.xmodem(data, size);
+                        case Crc16::X25: return fastcrc.x25(data, size);
+                    }
+                } else {
+                    switch (type) {
+                        case Crc16::CCITT: return fastcrc.ccitt_upd(data, size);
+                        case Crc16::KERMIT: return fastcrc.kermit_upd(data, size);
+                        case Crc16::MODBUS: return fastcrc.modbus_upd(data, size);
+                        case Crc16::XMODEM: return fastcrc.xmodem_upd(data, size);
+                        case Crc16::X25: return fastcrc.x25_upd(data, size);
+                    }
+                }
+                return 0xFFFF;
+            }
+
+            inline uint32_t crc32_update(const uint8_t* data, const size_t size, const Crc32 type = Crc32::CRC32) {
+                static FastCRC32 fastcrc;
+                static bool has_initialized {false};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    switch (type) {
+                        case Crc32::CRC32: return fastcrc.crc32(data, size);
+                        case Crc32::POSIX: return fastcrc.cksum(data, size);
+                    }
+                } else {
+                    switch (type) {
+                        case Crc32::CRC32: return fastcrc.crc32_upd(data, size);
+                        case Crc32::POSIX: return fastcrc.cksum_upd(data, size);
+                    }
+                }
+                return 0xFFFFFFFF;
+            }
+
         }  // namespace fastcrc
 
 #else
@@ -124,6 +184,57 @@ namespace util {
                 return CRCPP::CRC::Calculate(data, size, CRCPP::CRC::CRC_64());
             }
 
+            inline uint8_t crc8_update(const uint8_t* data, const size_t size, const Crc8 type = Crc8::SMBUS) {
+                static auto param = get_crc8_param(type);
+                static bool has_initialized {false};
+                static uint8_t result {0};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    result = CRCPP::CRC::Calculate(data, size, param);
+                } else {
+                    result = CRCPP::CRC::Calculate(data, size, param, result);
+                }
+                return result;
+            }
+
+            inline uint16_t crc16_update(const uint8_t* data, const size_t size, const Crc16 type = Crc16::MODBUS) {
+                static auto param = get_crc16_param(type);
+                static bool has_initialized {false};
+                static uint16_t result {0};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    result = CRCPP::CRC::Calculate(data, size, param);
+                } else {
+                    result = CRCPP::CRC::Calculate(data, size, param, result);
+                }
+                return result;
+            }
+
+            inline uint32_t crc32_update(const uint8_t* data, const size_t size, const Crc32 type = Crc32::CRC32) {
+                static auto param = get_crc32_param(type);
+                static bool has_initialized {false};
+                static uint32_t result {0};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    result = CRCPP::CRC::Calculate(data, size, param);
+                } else {
+                    result = CRCPP::CRC::Calculate(data, size, param, result);
+                }
+                return result;
+            }
+
+            inline uint64_t crc64_update(const uint8_t* data, const size_t size) {
+                static auto param = CRCPP::CRC::CRC_64();
+                static bool has_initialized {false};
+                static uint64_t result {0};
+                if (!has_initialized) {
+                    has_initialized = true;
+                    result = CRCPP::CRC::Calculate(data, size, param);
+                } else {
+                    result = CRCPP::CRC::Calculate(data, size, param, result);
+                }
+                return result;
+            }
         }  // namespace crcpp
 
 #endif
